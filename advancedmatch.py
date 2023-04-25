55bb532364f4a949e76b3476e0a887eefd24becd
Awonke Mnotoza <93478189+Awonke11@users.noreply.github.com>
2023-04-25 11:16:03 +0200

    question 3 and 4

---
+++ advancedmatch.py
@@ -0,0 +1,34 @@
+def advanceMatch(pattern, word):
+    if len(word) == 0 and len(pattern) == 0:
+        return True
+    
+    if len(word) == 0:
+        return False
+    
+    if (pattern[0:1] == "?"):
+        return advanceMatch(pattern[1:], word[1:])
+    
+    if (pattern[0:1] == "*"):
+        if pattern[-1:] == "*":
+            return True
+        if (pattern[-1:] != word[-1:]):
+            return False
+        else:
+            return advanceMatch(pattern[:-1], word[:-1])
+    
+    if (pattern[0:1] != word[0:1] or pattern[-1:] != word[-1:]):
+        return False
+    
+    return advanceMatch(pattern[1:-1], word[1:-1])
+
+loopingCondition = True
+while loopingCondition:
+    pattern = input("Enter a pattern (or 'q' to quit): \n")
+    if pattern == "q":
+        loopingCondition = False
+    else:
+        word = input("Enter a word: \n")
+        if advanceMatch(pattern, word):
+            print("It's a match.")
+        else:
+            print("They don't match.")
--- simplematch.py
+++ simplematch.py
@@ -1,28 +1,29 @@
 exitBoolean = True
 
 def checkMatch(pattern, word):
-    if (len(pattern) != len(word)):
+    if len(pattern) != len(word):
         return False
     
-    if (pattern[0:1] == "?"):
-        return checkMatch(pattern[1:], word[-1:])
+    if len(pattern) == 0:
+        return True
     
-    if (pattern[0:1] != word[-1:]):
+    if pattern[0:1] == "?":
+        return checkMatch(pattern[1:], word[1:])
+    
+    if pattern[0:1] != word[0:1] and pattern[-1:] != word[-1:]:
         return False;
     
-    return checkMatch(pattern[1:], word[-1:])
+    return checkMatch(pattern[1:-1], word[1:-1])
 
 while (exitBoolean):
     pattern = input("Enter a pattern (or 'q' to quit): \n")
     word = ""
-    if (pattern == "q"):
+    if pattern == "q":
         exitBoolean = False
     else:
         word = input("Enter a word: \n")
-        if (checkMatch(pattern, word)):
+        if checkMatch(pattern, word):
             print("It's a match.")
         else:
             print("They don't match.")
     
-
-
